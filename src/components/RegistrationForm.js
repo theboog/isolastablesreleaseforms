e013dfde102966df740f61a51c0a53dfe68c5f9a
Chris Larsen <c.r.larsen@hotmail.com>
2019-05-08 13:19:28 -0600

    merged

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -44,6 +44,25 @@ class RegistrationForm extends React.Component {
       });
   };
 
+  componentDidMount() {
+    // 1. Load the JavaScript client library.
+    window.gapi.load("client", this.initClient);
+  }
+
+  initClient = () => {
+    // 2. Initialize the JavaScript client library.
+    window.gapi.client
+      .init({
+        apiKey: config.apiKey,
+        // Your API key will be automatically added to the Discovery Document URLs.
+        discoveryDocs: config.discoveryDocs
+      })
+      .then(() => {
+        // 3. Initialize and make the API request.
+        load(this.onLoad);
+      });
+  };
+
   render() {
     const {
       firstName,
